a946dfd620f06bf4c82f75c4b41d8e6d653ef41e
tjsego <tjsego@gmail.com>
2024-02-03 04:36:38 -0500

    Added environment recipe

--- setup/install_env.bat
+++ setup/install_env.bat
@@ -1,7 +1,4 @@
 @echo off
 
 call conda env remove -n stoch_repro
-call conda create -y -n stoch_repro
-call conda activate stoch_repro
-call conda install -y python=3.10 scipy matplotlib notebook ipywidgets
-pip install antimony libroadrunner
+call conda env create -n stoch_repro -f %~dp0env.yml
--- setup/install_env.sh
+++ setup/install_env.sh
@@ -4,7 +4,4 @@ this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" ; pwd -P )"
 source ${HOME}/miniconda3/etc/profile.d/conda.sh
 
 conda env remove -n stoch_repro
-conda create -y -n stoch_repro
-conda activate stoch_repro
-conda install -y python=3.10 scipy matplotlib notebook ipywidgets numba ipympl
-pip install antimony libroadrunner
+conda env create -n stoch_repro -f ${this_dir}/env.yml
