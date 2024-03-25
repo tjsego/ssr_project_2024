9aa3443b5d4e05de884586181f519f4bf8aaaf6e
tjsego <tjsego@gmail.com>
2024-03-25 05:21:43 -0400

    Improved comparison metric

--- code/test_comparison.py
+++ code/test_comparison.py
@@ -70,6 +70,7 @@ class ComparisonResult:
         self.self_sim: Optional[Tuple[float, float]] = None
         self.comparison_results: List[Dict[str, np.ndarray]] = []
         self.comparison_error: List[Dict[str, float]] = []
+        self.comparison_pvals: List[Dict[str, float]] = []
         self.eval_info: Optional[Dict[str, List[Tuple[int, float]]]] = None
 
         if exec_start:
@@ -84,15 +85,16 @@ class ComparisonResult:
                 for name in var_names:
                     self.results[name][j, :] = sim_lib.round_arr_to_sigfigs(extract_rr_results(res, name), sig_figs)
             self.self_sim_evals = sim_lib.test_sampling(self.results, err_thresh=sampling_err_thresh)[0]
-            self.self_sim = np.average(self.self_sim_evals), np.std(self.self_sim_evals)
+            self.self_sim = np.average(self.self_sim_evals), np.std(self.self_sim_evals, ddof=1)
 
-            results_half = {k: v[:int(sample_size/2), :] for k, v in self.results.items()}
+            results_hsize = int(sample_size/2)
+            results_half = {k: v[:results_hsize, :] for k, v in self.results.items()}
             results_ecfs, _, self.eval_info = sim_lib.find_ecfs(results_half)
             for f in beta_facts:
                 dist_info = {param_name: (param_dist_name, (param_dist_args[0] * f, param_dist_args[1] * f))}
                 rr = sim_lib.make_rr(model_string, stochastic)
-                results_comp = {n: np.zeros((sample_size, num_steps)) for n in var_names}
-                for j, res in enumerate(sim_lib.exec_rr_batch(sample_size, rr, t_fin, num_steps, stochastic, dists=dist_info)):
+                results_comp = {n: np.zeros((results_hsize, num_steps)) for n in var_names}
+                for j, res in enumerate(sim_lib.exec_rr_batch(results_hsize, rr, t_fin, num_steps, stochastic, dists=dist_info)):
                     for name in var_names:
                         results_comp[name][j, :] = sim_lib.round_arr_to_sigfigs(extract_rr_results(res, name), sig_figs)
 
@@ -100,6 +102,7 @@ class ComparisonResult:
                 self.comparison_results.append(results_comp)
 
                 self.comparison_error.append(dict())
+                self.comparison_pvals.append(dict())
                 for n in var_names:
                     err_max = 0.0
                     for i in range(num_steps):
@@ -107,6 +110,15 @@ class ComparisonResult:
                                                sim_lib.get_eval_info_times(*self.eval_info[n][i]))
                         err_max = max(err_max, sim_lib.ecf_compare(results_ecfs[n][i], ecf_comp))
                     self.comparison_error[-1][n] = err_max
+                    if err_max < self.self_sim[0]:
+                        pval = 1.0
+                    else:
+                        q2 = (results_hsize + 1) / results_hsize * self.self_sim[1] * self.self_sim[1]
+                        lam2 = ((err_max - self.self_sim[0]) ** 2) / q2
+                        pval = np.floor((results_hsize + 1) / results_hsize * ((results_hsize - 1) / lam2 + 1)) / (results_hsize + 1)
+                        if pval > 1:
+                            pval = 1.0
+                    self.comparison_pvals[-1][n] = pval
 
     def to_json(self):
         if self.results_times is None:
@@ -126,6 +138,7 @@ class ComparisonResult:
             self_sim=self.self_sim,
             comparison_results=[{k: v.tolist() for k, v in e.items()} for e in self.comparison_results],
             comparison_error=self.comparison_error,
+            comparison_pvals=self.comparison_pvals,
             eval_info=self.eval_info
         )
 
@@ -139,13 +152,14 @@ class ComparisonResult:
         result.sig_figs = int(data['sig_figs'])
         result.sampling_err_thresh = float(data['sampling_err_thresh'])
         result.results_times = np.asarray(data['results_times'], dtype=float)
-        result.dist_info = {k: (v[0], (float(v[1][0]), float(v[1][1]))) for k, v in data['dist_info'].items()}
+        result.dist_info = [{k: (v[0], (float(v[1][0]), float(v[1][1]))) for k, v in e.items()} for e in data['dist_info']]
         result.results = {k: np.asarray(v, dtype=float) for k, v in data['results'].items()}
         result.self_sim_evals = [float(v) for v in data['self_sim_evals']]
         result.self_sim = float(data['self_sim'][0]), float(data['self_sim'][1])
         result.comparison_results = [{k: np.asarray(v, dtype=float) for k, v in e.items()}
                                      for e in data['comparison_results']]
         result.comparison_error = [{k: float(v) for k, v in e.items()} for e in data['comparison_error']]
+        result.comparison_pvals = [{k: float(v) for k, v in e.items()} for e in data['comparison_pvals']]
         result.eval_info = {k: [(int(vv[0]), float(vv[1])) for vv in v] for k, v in data['eval_info'].items()}
 
         return result
